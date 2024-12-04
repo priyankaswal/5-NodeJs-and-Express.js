1bbfdf327fe7cf206f14470542369cae99fe0d7b
Priyank Aswal <aswalpriyank0@gmail.com>
2024-12-04 16:40:07 +0530

    class 82

---
+++ 14 cookies and sessions/app.js
@@ -0,0 +1,64 @@
+require("dotenv").config();
+const path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser");
+const rootDir = require("./util/path-util");
+const mongoose = require("mongoose");
+const { authRouter } = require("./routers/authRouter");
+const session = require("express-session");
+const { hostRouter } = require("./routers/hostRouter");
+const storeRouter = require("./routers/storeRouter");
+const errorController = require("./controllers/errorController");
+const { collection } = require("./models/Favourite");
+const mongodb_session = require('connect-mongodb-session');
+
+const MongoDBStore = mongodb_session(session);
+
+const MONGO_DB_URL = process.env.MONGO_DB_URL;
+const SESSION_SECRET = process.env.SESSION_SECRET;
+
+const sessionStore = new MongoDBStore({
+  uri: MONGO_DB_URL,
+  collection: 'sessions',
+})
+
+const app = express();
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+app.use(express.static(path.join(rootDir, "public")));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(
+  session({
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    store:sessionStore,
+  })
+);
+
+// app.use((req, res, next) => {
+//   req.isLoggedIn = req.get("Cookie").split("=")[1] === "true";
+//   next();
+// });
+
+app.use(storeRouter);  
+app.use("/host", (req, res, next) => {
+  if (!req.session.isLoggedIn) {
+    return res.redirect("/login");
+  }
+  next();
+});
+
+app.use("/host", hostRouter);
+app.use(authRouter);
+app.use(errorController.get404);
+
+const PORT = 3000;
+
+mongoose.connect(MONGO_DB_URL).then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+  });
+});
---
+++ 14 cookies and sessions/controllers/storeController.js
@@ -0,0 +1,80 @@
+const mongoose = require("mongoose");
+const Favourite = require("../models/Favourite");
+const Home = require("./../models/Home");
+
+exports.getIndex = (req, res, next) => {
+  console.log(req.session);
+
+  Home.find().then((registeredHomes) => {
+    res.render("store/index", {
+      homes: registeredHomes,
+      pageTitle: "Hmara Airbnb",
+      isLoggedIn: req.session.isLoggedIn,
+    });
+  });
+};
+
+exports.getHomes = (req, res, next) => {
+  Home.find().then((registeredHomes) => {
+    res.render("store/homes", {
+      homes: registeredHomes,
+      pageTitle: "Hmara Airbnb",
+      isLoggedIn: req.session.isLoggedIn,
+    });
+  });
+};
+
+exports.getFavourites = (req, res, next) => {
+  Favourite.find()
+    .populate("homeId")
+    .then((favIdHomes) => {
+      const favouriteHomes = favIdHomes.map((favIdHome) => favIdHome.homeId);
+      res.render("store/favourites", {
+        homes: favouriteHomes,
+        pageTitle: "Favourites",
+        isLoggedIn: req.session.isLoggedIn,
+      });
+    });
+};
+
+exports.postAddFavourites = (req, res, next) => {
+  const homeId = req.body.id;
+  const fav = new Favourite({ homeId });
+
+  fav
+    .save()
+    .then(() => {
+      res.redirect("/favourites");
+    })
+    .catch((error) => {
+      console.log("Error while adding to favourites", error);
+      res.redirect("/favourites");
+    });
+};
+
+exports.postRemoveFavourite = (req, res, next) => {
+  const homeId = req.params.homeId;
+
+  Favourite.findOneAndDelete({ homeId })
+    .then(() => {
+      res.redirect("/favourites");
+    })
+    .catch((error) => {
+      console.log("Error while remove from favourites", error);
+      res.redirect("/favourites");
+    });
+};
+
+exports.getHomeDetails = (req, res, next) => {
+  const homeId = req.params.homeId;
+  Home.findById(homeId).then((home) => {
+    if (!home) {
+      return res.redirect("/homes");
+    }
+    res.render("store/home-detail", {
+      home: home,
+      pageTitle: "Home Detail",
+      isLoggedIn: req.session.isLoggedIn,
+    });
+  });
+};
